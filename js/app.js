bf8a6aa537f7fcf310a948e453791a4c73cc5143
Nayeem547 <106963106+Nayeem547@users.noreply.github.com>
2023-01-27 22:52:32 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,8 @@
+
+$(".banner_slider").slick({
+    arrows: true,
+    speed: 1500,
+    prevArrow: '<i class="fa-solid fa-chevron-left banner_slider_icon_left"></i>',
+    nextArrow: '<i class="fa-solid fa-chevron-right banner_slider_icon_right"></i>',
+    fade:true,
+  });
